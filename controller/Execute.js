89d4b2b3e01f130ee07644dc08b7ac5ec2859da8
Danielgol <danielgol@yahoo.com.br>
2017-09-30 11:00:50 -0300

    Fazendo o game over

--- controller/Execute.js
+++ controller/Execute.js
@@ -24,8 +24,8 @@ function loop(){
 						var response = new SAT.Response();
 						var collided = SAT.testCircleCircle(molecules[i].circle, shots[x].circle, response);// VERIFICA A COLISÃO
 						if(collided === true){//	SE COLIDIU...
-							molecules.splice(i, 1);//	REMOVA A MOLECULA
-							shots.splice(x, 1);//	REMOVA O TIRO
+							molecules.splice(i, 1);//	REMOVE A MOLECULA
+							shots.splice(x, 1);//	REMOVE O TIRO
 							score.increaseScore(10);// AUMENTA O SCORE
 						}
 				}
@@ -34,12 +34,48 @@ function loop(){
 				var response = new SAT.Response();
 				var collided = SAT.testPolygonCircle(ship.triangle, molecules[i].circle, response);// VERIFICA A COLISÃO
 				if(collided === true){//	SE COLIDIU...
-					molecules.splice(i, 1);//	REMOVA A MOLECULA
+					molecules.splice(i, 1);//	REMOVE A MOLECULA
 					ship.respawn(canvas.width/2, canvas.height/2);// FAZ COM QUE A NAVE VOLTE PARA O CENTRO
 				}
 		}
 
-		drawScore(ctx, score.score, (canvas.width/2)-5, 20);
+		drawScore(ctx, score.score, (canvas.width/2)-5, 20);// EXIBE O SCORE ATUAL
+
+
+		//X---ALTERAR TUDO ABAIXO DEPOIS--------------------------------------------------------------------------X
+
+		//	A CONDIÇÃO DO FIM SERÁ ALTERADA DEPOIS PARA: SE A VIDA DA NAVE FOR IGUAL A ZERO
+		//	SALVAR AS COISAS A PARTIR DAQUI
+		if(molecules.length === 0){// CONDIÇÃO DO FIM
+
+			clearInterval(IntervalId);// INTERROMPE O LOOP;
+
+			//	FAZ A MINI "TELINHA" DE SUBMISSÃO
+			ctx.beginPath();ctx.lineWidth = 2;
+			ctx.moveTo((canvas.width/2)-250, (canvas.height/2)-150 );
+			ctx.lineTo((canvas.width/2)-250, (canvas.height/2)+150 );
+			ctx.lineTo((canvas.width/2)+250, (canvas.height/2)+150 );
+			ctx.lineTo((canvas.width/2)+250, (canvas.height/2)-150 );
+			ctx.lineTo((canvas.width/2)-250, (canvas.height/2)-150 );
+			ctx.strokeStyle = "white";
+			ctx.stroke();ctx.closePath();
+			ctx.clearRect((canvas.width/2)-250, (canvas.height/2)-150, 500, 300);
+			ctx.beginPath();
+		  ctx.fillStyle = "white";
+		  ctx.font = "20px Arial";
+		  ctx.fillText("FIM DO JOGO!", (canvas.width/2)-70, (canvas.height/2)-100);
+		  ctx.closePath();
+
+			//	TORNA O CAMPO DE TEXTO VISIVEL: https://stackoverflow.com/questions/3961422/conditional-display-of-html-element-forms
+			document.getElementById("nameBox").style.visibility="visible";
+
+			//ISSUE:	Aparecer o campo para digitar o nome + submit + validação (nome vazio, caracteres..., fazer depois)
+			//ISSUE:	Salvar o score no banco
+			//ISSUE: 	Quit (reiniciar sem salvar o score, o reiniciar é para depois)
+
+		}
+
+		//X-------------------------------------------------------------------------------------------------------X
 
 }
 
@@ -52,16 +88,16 @@ function start(){
 		molecules.push(createMolecule(canvas.width, canvas.height));
 		molecules.push(createMolecule(canvas.width, canvas.height));
 
+		ship = createShip(canvas.width/2, canvas.height/2);
+
 		score = new Score();// Data? lugar? ...
+
+		//ZERAR AS COISAS (para que as coisas do jogo anterior não continuem, ex: moleculas, tiros, ...):
+		//molecules, shots, IntervalId, ...
 
 		drawScreen(ctx, canvas.width, canvas.height);
 
-		setInterval(loop, 5);
-
-		//https://stackoverflow.com/questions/109086/stop-setinterval-call-in-javascript/109098
-		//var IntervalId; colocar como global
-		//IntervalId = setInterval(loop, 5); colocar aqui
-		//clearInterval(IntervalId); colocar na condição para parar o jogo
+		IntervalId = setInterval(loop, 5);//https://stackoverflow.com/questions/109086/stop-setinterval-call-in-javascript/109098
 
 }
 
@@ -73,7 +109,8 @@ var ctx = canvas.getContext("2d");
 var molecules = [];
 var shots = [];
 var score;
-var ship = createShip(canvas.width/2, canvas.height/2);
+var IntervalId;
+var ship;
 
 
 start();
