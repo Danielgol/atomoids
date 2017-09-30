285d8a067b93bf1815f8a182edc2126350f13efd
@AdonisBelo <adonisb2012@gmail.com>
2017-09-30 10:23:03 -0300

    Iniciando atividades com círculos

--- controller/Executar.js
+++ controller/Executar.js
@@ -5,7 +5,11 @@ function loopDoJogo(){
 
 	comandosTeclado(nave);
 
+	moleculas[0].mover();
+
 	desenharNave(ctx, nave.triangulo);
+	
+	desenharMolecula(ctx, moleculas[0].circulo);
 
 }
 
@@ -19,6 +23,20 @@ function jogo(ctx, height, width){
 	nave.triangulo.rotate((Math.PI/180)*180);
 	nave.triangulo.translate(width/2, height/2);
 
+
+
+	var circulo = new SAT.Circle(new SAT.Vector(Math.floor((Math.random() * width-1) + 1),  
+	Math.floor((Math.random() * height-1) + 1)), Math.floor((Math.random() * 30) + 10));
+
+	//var circulo = new SAT.Circle(new SAT.Vector(500, 300), 10);	
+
+	var molecula = new Molecula(circulo, Math.floor((Math.random() * 359) + 1));
+
+	moleculas.push(molecula);
+
+
+
+
 	desenharLimite(ctx, height, width);
 
 	setInterval(loopDoJogo, 5);
@@ -28,6 +46,8 @@ function jogo(ctx, height, width){
 var canvas = document.getElementById("mycanvas");
 
 var ctx = canvas.getContext("2d");
+
+var moleculas = [];
 
 var nave;
 
