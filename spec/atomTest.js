873dfee53d4241e49f4cbf12304f74e7c7f00237
@AdonisBelo <adonisb2012@gmail.com>
2017-11-18 22:23:28 -0200

    Adicionando teste da posição do átomo quando o X é igual a 0

--- spec/atomTest.js
+++ spec/atomTest.js
@@ -1230,4 +1230,16 @@ describe("Atom Test", function() {
 
   });
 
+  it("test atom position when X is equal to 0 ", function(){
+
+    circle = new SAT.Circle(new SAT.Vector(0, 300), 25);
+    atom = new Atom(circle, 1, "blue");
+    
+
+    atom.obeyLimit(width, height);
+
+    expect(0).toBe(atom.circle['pos'].x);
+
+  });
+
   });
